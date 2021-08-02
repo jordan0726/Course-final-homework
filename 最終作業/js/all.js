f0a6ffa9ba30544c1900a786b3b9966b75d69a95
jordan0726 <52375453+jordan0726@users.noreply.github.com>
2021-08-02 11:05:54 +0800

    Delete 最終作業/js directory

--- 最終作業/js/all.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $(".navbar_mb-option img").on('click',(function (e) { 
-        e.preventDefault();
-        $('.navbar_mb-menu').toggleClass('menu-show');
-    }));
-});
